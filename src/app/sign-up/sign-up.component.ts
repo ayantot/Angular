f190bb305cbe156a46dc1dc3b2fcebee4de808d3
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-03-19 18:00:31 +0100

    Angular2

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Signup } from '../sign-up';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  mail : Signup = {
+    email: "Mon email"
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
