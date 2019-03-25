fc3d36fa95c82544931552c01b778d889900dbea
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-03-25 12:10:56 +0100

    les directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ShowMoviesDirective } from './show-movies.directive';
+import { HightLightDirective } from './hight-light.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ShowMoviesDirective,
+    HightLightDirective
   ],
   imports: [
     BrowserModule,
